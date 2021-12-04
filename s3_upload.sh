ffbaba030029d7d90483401be5baf08ef20c90e8
elakia <vmelakia@gmail.com>
2021-12-04 20:44:35 +0530

    time for the csv is added

--- s3_upload.sh
+++ s3_upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 cp info.csv s3://neighbour-data/neighbour-$(date +%F)-$(date +%H).csv
+aws s3 cp info.csv s3://neighbour-data/neighbour-$(date +%F)-$(date +%H)-$(date +"%T").csv.csv
